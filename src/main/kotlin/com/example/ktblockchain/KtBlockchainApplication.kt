78f339c852e661a5903a63943c23eda1b80ba67f
Yu-s <yuuu1993g@gmail.com>
2020-11-24 11:33:41 +0900

    first commit

---
+++ src/main/kotlin/com/example/ktblockchain/KtBlockchainApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ktblockchain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtBlockchainApplication
+
+fun main(args: Array<String>) {
+	runApplication<KtBlockchainApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/ktblockchain/KtBlockchainApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ktblockchain
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtBlockchainApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
